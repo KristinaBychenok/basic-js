dad2de6399b2665f8fe9f5fb3110297f8cdb5b9b
KristinaBychenok <77573069+KristinaBychenok@users.noreply.github.com>
2021-03-16 00:25:14 +0300

    Feat: complete tast count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let arr = matrix.reduce(function(acc, item) {
+    let result = item.filter((cats) => cats == '^^')
+    return [...acc, ...result];
+  }, []);
+  return arr.length
 };
